fe7b2ace83ceb2fe3be3c00177d27506f49a6c1a
malikyasir8801-cmd <malikyasir8801@gmail.com>
2026-01-11 15:57:23 +0100

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,12 @@
 def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheck(
+    additionalArguments: '''
+      --scan ./
+      --data /var/lib/jenkins/.dependency-check
+      --format XML
+    ''',
+    odcInstallation: 'OWASP'
+  )
+
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
